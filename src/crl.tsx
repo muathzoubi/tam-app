66d686c9ca644490da85b7fb0b833e046efade7d
muathzoubi <moathaza@gmail.com>
2024-03-21 01:23:33 +0300

    a

--- src/crl.tsx
+++ src/crl.tsx
@@ -38,15 +38,7 @@ const Coursol = () => {
             Go to next slide
           </a>
         </li>
-        <li id="carousel__slide4" tabIndex={0} className="carousel__slide">
-          <div className="carousel__snapper"></div>
-          <a href="#carousel__slide3" className="carousel__prev">
-            Go to previous slide
-          </a>
-          <a href="#carousel__slide1" className="carousel__next">
-            Go to first slide
-          </a>
-        </li>
+     
       </ol>
       <aside className="carousel__navigation">
         <ol className="carousel__navigation-list">
